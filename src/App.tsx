8dfc1122e0b28b303cbe0463552fa5340601b42a
danielmpadua <danielmarianopadua@gmail.com>
2024-12-02 03:24:02 -0300

    change elements UI, added appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import { Box, Button, Typography } from "@mui/material";
-import { useContext, useEffect } from "react";
+import { AppBar, Box, MenuItem, Select, Typography } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { ThemeContext } from "./contexts/ThemeContext";
 import { DEFAULT_LANGUAGE } from "./languages/i18n";
 import { AnimatedFooter } from "./components/AnimatedFooter";
 import HandymanOutlinedIcon from "@mui/icons-material/HandymanOutlined";
+import { ThemeSwitch } from "./components/ThemeSwitch";
+import { useDanielAnimation } from "./hooks/useDaniAnimation";
+import { useScreenSize } from "./hooks/useScreenSize";
 
 function App() {
   const { isLightMode, changeThemeMode } = useContext(ThemeContext);
+  const { width } = useScreenSize();
+  const { animationHeight } = useDanielAnimation({ width });
+
+  const countries = ["BR", "US"];
+
+  const [country, setCountry] = useState<string>(countries[0]);
   const {
     t,
-    i18n: { language, changeLanguage },
+    i18n: { changeLanguage },
   } = useTranslation();
 
   useEffect(() => {
     changeLanguage(sessionStorage.getItem("language") || DEFAULT_LANGUAGE);
+    setCountry(
+      sessionStorage.getItem("language") === "en" ? countries[1] : countries[0]
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -22,9 +34,9 @@ function App() {
     <Box
       sx={{
         width: "100%",
-        background: isLightMode
-          ? "linear-gradient(180deg, rgba(82,204,255,1) 19%, rgba(25,182,247,1) 70%)"
-          : "linear-gradient(0deg, rgba(40,59,70,1) 19%, rgba(14,20,24,1) 70%)",
+        background: isLightMode ? "#19B6F7" : "#0E1418",
+        // ? "linear-gradient(180deg, rgba(82,204,255,1) 19%, rgba(25,182,247,1) 70%)"
+        // : "linear-gradient(0deg, rgba(40,59,70,1) 19%, rgba(14,20,24,1) 70%)",
         height: "100vh", // vh fallback
         // @ts-ignore
         height: "100dvh",
@@ -33,56 +45,93 @@ function App() {
         position: "relative",
       }}
     >
+      <AppBar
+        position="static"
+        sx={{
+          background: isLightMode ? "#41b0a0" : "#426274",
+          borderBottom: `2px solid ${isLightMode ? "#177864" : "#244955"}`,
+          zIndex: 3,
+          padding: 1,
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            gap: 2,
+          }}
+        >
+          <Typography>{t("change_language")}:</Typography>
+
+          <Select
+            value={country}
+            inputProps={{
+              sx: {
+                padding: "5px 12px 3px 12px",
+                margin: 0,
+                background: "white",
+              },
+            }}
+            onChange={(e) => {
+              const newLanguage = e.target.value === "BR" ? "pt" : "en";
+              changeLanguage(newLanguage);
+              sessionStorage.setItem("language", newLanguage);
+              setCountry(
+                countries.find((c) => c === e.target.value) || countries[0]
+              );
+            }}
+          >
+            {countries.map((ctr) => (
+              <MenuItem value={ctr}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+                  <img
+                    loading="lazy"
+                    style={{ width: 20 }}
+                    srcSet={`https://flagcdn.com/w40/${ctr.toLowerCase()}.png 2x`}
+                    src={`https://flagcdn.com/w40/${ctr.toLowerCase()}.png`}
+                    alt={ctr}
+                  />
+                  <Typography>{ctr}</Typography>
+                </Box>
+              </MenuItem>
+            ))}
+          </Select>
+
+          <ThemeSwitch
+            checked={!isLightMode}
+            onChange={() => changeThemeMode()}
+          />
+        </Box>
+      </AppBar>
       <Box
         sx={{
-          height: "100%",
+          height: `calc(100% - ${animationHeight}px)`,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          gap: 2,
+          width: "100%",
+          zIndex: 1,
         }}
       >
-        <Button
-          variant="contained"
-          onClick={changeThemeMode}
-          sx={{ width: "fit-content", background: "#426274" }}
-        >
-          {isLightMode ? t("dark_theme") : t("light_theme")}
-        </Button>
-        <Button
-          sx={{ width: "fit-content", background: "#426274" }}
-          variant="contained"
-          onClick={() => {
-            const newLanguage = language === "pt" ? "en" : "pt";
-            changeLanguage(newLanguage);
-            sessionStorage.setItem("language", newLanguage);
-          }}
-        >
-          {t("change_language")}
-        </Button>
-
-        <Box
+        <HandymanOutlinedIcon
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
+            height: 150,
+            width: 150,
+            color: "#426274",
           }}
-        >
-          <HandymanOutlinedIcon
-            sx={{
-              height: 200,
-              width: 200,
-              color: "#426274",
-            }}
-          />
+        />
 
-          <Typography variant="h2" color={"#426274"}>
-            {t("under_construction")} :)
-          </Typography>
-        </Box>
+        <Typography
+          variant="h3"
+          fontFamily="Pixelify Sans"
+          fontWeight={700}
+          color={"#426274"}
+        >
+          {t("under_construction").toUpperCase()}
+        </Typography>
       </Box>
 
       <AnimatedFooter />
