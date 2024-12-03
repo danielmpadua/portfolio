2ab7260c7d3fd5623adf5e9edd507c5545e01899
danielmpadua <danielmarianopadua@gmail.com>
2024-12-03 13:36:13 -0300

    Improving the Components folder structure and making AppBar a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,23 @@
-import { AppBar, Box, MenuItem, Select, Typography } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Box, Typography } from "@mui/material";
+import { useContext } from "react";
 import { ThemeContext } from "./contexts/ThemeContext";
-import { DEFAULT_LANGUAGE } from "./languages/i18n";
 import HandymanOutlinedIcon from "@mui/icons-material/HandymanOutlined";
-import { ThemeSwitch } from "./components/ThemeSwitch";
 import { useDanielAnimation } from "./hooks/useDaniAnimation";
 import { useScreenSize } from "./hooks/useScreenSize";
 import { AppContainer } from "./components/AppContainer";
 import { FooterAnimation } from "./components/FooterAnimation";
-
-const APP_BAR_HEIGHT = 52;
+import { APP_BAR_HEIGHT, HeaderMenu } from "./components/HeaderMenu";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  const { isLightMode, changeThemeMode } = useContext(ThemeContext);
+  const { isLightMode } = useContext(ThemeContext);
   const { width } = useScreenSize();
   const { animationHeight } = useDanielAnimation({ width });
-
-  const countries = ["BR", "US"];
-
-  const [country, setCountry] = useState<string>(countries[0]);
-  const {
-    t,
-    i18n: { changeLanguage },
-  } = useTranslation();
-
-  useEffect(() => {
-    changeLanguage(localStorage.getItem("language") || DEFAULT_LANGUAGE);
-    setCountry(
-      localStorage.getItem("language") === "en" ? countries[1] : countries[0]
-    );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { t } = useTranslation();
 
   return (
     <AppContainer isLightMode={isLightMode}>
-      <AppBar
-        position="static"
-        sx={{
-          background: isLightMode ? "#41b0a0" : "#426274",
-          borderBottom: `2px solid ${isLightMode ? "#177864" : "#244955"}`,
-          zIndex: 3,
-          padding: 1,
-        }}
-      >
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            gap: 2,
-          }}
-        >
-          <Typography>{t("change_language")}:</Typography>
-
-          <Select
-            value={country}
-            inputProps={{
-              sx: {
-                padding: "5px 12px 3px 12px",
-                margin: 0,
-                background: "white",
-              },
-            }}
-            onChange={(e) => {
-              const newLanguage = e.target.value === "BR" ? "pt" : "en";
-              changeLanguage(newLanguage);
-              localStorage.setItem("language", newLanguage);
-              setCountry(
-                countries.find((c) => c === e.target.value) || countries[0]
-              );
-            }}
-          >
-            {countries.map((ctr) => (
-              <MenuItem key={ctr} value={ctr}>
-                <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
-                  <img
-                    loading="lazy"
-                    style={{ width: 20 }}
-                    srcSet={`https://flagcdn.com/w40/${ctr.toLowerCase()}.png 2x`}
-                    src={`https://flagcdn.com/w40/${ctr.toLowerCase()}.png`}
-                    alt={ctr}
-                  />
-                  <Typography>{ctr}</Typography>
-                </Box>
-              </MenuItem>
-            ))}
-          </Select>
-
-          <ThemeSwitch
-            checked={!isLightMode}
-            onChange={() => changeThemeMode()}
-          />
-        </Box>
-      </AppBar>
+      <HeaderMenu />
 
       <Box
         sx={{
