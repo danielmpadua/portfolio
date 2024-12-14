d7b66f528349b5aec6e4c340eed8837e3a3092bf
danielmpadua <danielmarianopadua@gmail.com>
2024-12-13 23:27:21 -0300

    fix git pages routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,9 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import { Portifolio, NotFound } from "../pages";
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<Navigate to={"portifolio"} replace />} />
-    <Route path="/portifolio" element={<Portifolio />} />
+    <Route path="/" element={<Portifolio />} />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
