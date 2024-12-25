143c62a2a9595050981f6f3c4703b2701d2606b5
Daniel <danielmarianopadua@gmail.com>
2024-12-25 06:28:51 -0300

    change taboe data structure

---
+++ src/pages/Finance/components/DistributionTable/IncomeRow.tsx
@@ -0,0 +1,155 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import {
+  handlePercentageLimit,
+  handleValueLimit,
+  ruleOfThree,
+} from "../../../../utils/finance";
+import {
+  Collapse,
+  IconButton,
+  Table,
+  TableCell,
+  TableRow,
+} from "@mui/material";
+import { PercentageInput } from "../../../../components/PercentageInput";
+import { CurrencyInput } from "../../../../components/CurrencyInput";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { TRowContent } from ".";
+
+export const IncomeRow = ({
+  row,
+  availableCash,
+  currentIndex,
+  handleChangeContent,
+  layer = 1,
+  parentsIndex,
+}: {
+  row: TRowContent;
+  availableCash?: number;
+  currentIndex: number;
+  layer?: number;
+  parentsIndex?: number[];
+  handleChangeContent: (
+    content: TRowContent,
+    index: number,
+    parentsIndex?: number[]
+  ) => void;
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const onChangeIncomeValue = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const newValue = e.target.value as unknown as number;
+    const optionalValue = handleValueLimit(
+      isNaN(newValue) ? undefined : newValue,
+      availableCash,
+      0
+    );
+
+    handleChangeContent(
+      {
+        ...row,
+        value: optionalValue,
+        percentage: ruleOfThree(availableCash, 100, optionalValue),
+      },
+      currentIndex,
+      parentsIndex
+    );
+  };
+
+  const onChangeIncomePercentage = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const newValue = e.target.value as unknown as string;
+    const optionalValue = handlePercentageLimit(
+      isNaN(Number(newValue)) ? undefined : Number(newValue)
+    );
+
+    handleChangeContent(
+      {
+        ...row,
+        value: ruleOfThree(100, availableCash, optionalValue),
+        percentage: optionalValue,
+      },
+      currentIndex,
+      parentsIndex
+    );
+  };
+
+  useEffect(() => {
+    if (!!availableCash || availableCash === 0) {
+      handleChangeContent(
+        {
+          ...row,
+          value: ruleOfThree(100, availableCash, row?.percentage),
+          percentage: row?.percentage,
+        },
+        currentIndex,
+        parentsIndex
+      );
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [availableCash]);
+
+  return (
+    <>
+      <TableRow>
+        <TableCell sx={{ width: "33.3%", pl: layer }}>
+          <IconButton
+            disabled={!row?.items?.length}
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+            sx={{ mr: 1, opacity: !!row?.items?.length ? 1 : 0 }}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+          {row?.name}
+        </TableCell>
+
+        <TableCell align="center" sx={{ width: "33.3%" }}>
+          <PercentageInput
+            value={row?.percentage}
+            onChange={onChangeIncomePercentage}
+          />
+        </TableCell>
+        <TableCell align="center" sx={{ width: "33.3%" }}>
+          <CurrencyInput value={row?.value} onChange={onChangeIncomeValue} />
+        </TableCell>
+      </TableRow>
+
+      {row?.items?.length && (
+        <TableRow>
+          <TableCell style={{ padding: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Table
+                sx={{ minWidth: 650, padding: 0, margin: 0 }}
+                aria-label="Distribution Table"
+                size="small"
+              >
+                {row?.items?.map((item, index) => (
+                  <IncomeRow
+                    key={item?.name}
+                    row={item}
+                    availableCash={row?.value}
+                    currentIndex={index}
+                    handleChangeContent={handleChangeContent}
+                    layer={layer + 1}
+                    parentsIndex={
+                      !!parentsIndex
+                        ? [...parentsIndex, currentIndex]
+                        : [currentIndex]
+                    }
+                  />
+                ))}
+              </Table>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      )}
+    </>
+  );
+};
--- src/pages/Finance/components/DistributionTable/index.tsx
+++ src/pages/Finance/components/DistributionTable/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Paper,
   Table,
   TableBody,
@@ -13,6 +14,7 @@ import {
 import { IncomeTypesRow } from "./IncomeTypesRow";
 import { useState } from "react";
 import { TIncome } from "../..";
+import { IncomeRow } from "./IncomeRow";
 
 type TDistributionTable = {
   availableCash?: number;
@@ -22,10 +24,111 @@ export type TRowContent = {
   name: string;
   percentage?: number;
   value?: number;
+
   items?: TRowContent[];
 };
 
 export const DistributionTable = ({ availableCash }: TDistributionTable) => {
+  const [tableContent, setTableContent] = useState<TRowContent[]>([
+    {
+      name: "Renda Fixa",
+      percentage: 0,
+      value: 0,
+      items: [
+        {
+          name: "CDB",
+          percentage: 0,
+          value: 0,
+        },
+        {
+          name: "LCI",
+          percentage: 0,
+          value: 0,
+        },
+        {
+          name: "LCA",
+          percentage: 0,
+          value: 0,
+        },
+        {
+          name: "Tesouro Direto",
+          percentage: 0,
+          value: 0,
+        },
+      ],
+    },
+    { name: "Renda Variavel", percentage: 0, value: 0 },
+  ]);
+
+  const handleRecursiveChange = (
+    row: TRowContent,
+    content: TRowContent,
+    currentIndex: number,
+    parentsIndex?: number[]
+  ) => {
+    console.log("handleRecursiveChange");
+    console.log("row:", row);
+    console.log("====");
+
+    if (parentsIndex?.length) {
+      const newRow: TRowContent = {
+        ...row,
+        items: row?.items?.map((item, index) => {
+          if (index === parentsIndex[0])
+            return handleRecursiveChange(
+              item,
+              content,
+              index,
+              parentsIndex?.filter((_, i) => i !== 0)
+            );
+          return item;
+        }),
+      };
+
+      return newRow;
+    }
+
+    return content;
+  };
+
+  const handleChangeContent = (
+    content: TRowContent,
+    currentIndex: number,
+    parentsIndex?: number[]
+  ) => {
+    console.log("content:", content);
+    console.log("currentIndex:", currentIndex);
+    console.log("parentsIndex:", parentsIndex);
+
+    setTableContent((prev) =>
+      prev?.map((prevRow, prevIndex) => {
+        if (parentsIndex?.length) {
+          if (prevIndex === parentsIndex[0])
+            return {
+              ...prevRow,
+              items: prevRow?.items?.map((item, index) => {
+                if (index === currentIndex)
+                  return handleRecursiveChange(
+                    item,
+                    content,
+                    index,
+                    parentsIndex?.filter((_, i) => i !== 0)
+                  );
+                console.log("caiu aqui");
+
+                return item;
+              }),
+            };
+
+          return prevRow;
+        } else {
+          if (prevIndex === currentIndex) return content;
+          return prevRow;
+        }
+      })
+    );
+  };
+
   const [fixedIncome, setFixedIncome] = useState<TIncome>({});
   const [variableIncome, setVariableIncome] = useState<TIncome>({});
   const [cdb, setCdb] = useState<TIncome>({});
@@ -47,184 +150,222 @@ export const DistributionTable = ({ availableCash }: TDistributionTable) => {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table
-        sx={{ minWidth: 650 }}
-        aria-label="Distribution Table"
-        size="small"
-      >
-        <TableHead sx={{ background: "#e3e3e3" }}>
-          <TableRow>
-            <TableCell sx={{ width: "33.3%" }}>Tipo de investimento</TableCell>
-            <TableCell align="center" sx={{ width: "33.3%" }}>
-              % a ser investido
-            </TableCell>
-            <TableCell align="center" sx={{ width: "33.3%" }}>
-              Valor a ser investido
-            </TableCell>
-          </TableRow>
-        </TableHead>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ minWidth: 650 }}
+          aria-label="Distribution Table"
+          size="small"
+        >
+          <TableHead sx={{ background: "#e3e3e3" }}>
+            <TableRow>
+              <TableCell sx={{ width: "33.3%" }}>
+                Tipo de investimento
+              </TableCell>
+              <TableCell align="center" sx={{ width: "33.3%" }}>
+                % a ser investido
+              </TableCell>
+              <TableCell align="center" sx={{ width: "33.3%" }}>
+                Valor a ser investido
+              </TableCell>
+            </TableRow>
+          </TableHead>
 
-        <TableBody>
-          <IncomeTypesRow
-            name="Renda Fixa"
-            income={fixedIncome}
-            setIncome={setFixedIncome}
-            availableCash={availableCash}
-          >
-            <TableRow sx={{ background: "whitesmoke" }}>
-              <TableCell sx={{ width: "33.3%", pl: 7 }}>
-                <Typography fontWeight={700} color="grey">
+          <TableBody>
+            <IncomeTypesRow
+              name="Renda Fixa"
+              income={fixedIncome}
+              setIncome={setFixedIncome}
+              availableCash={availableCash}
+            >
+              <TableRow sx={{ background: "whitesmoke" }}>
+                <TableCell sx={{ width: "33.3%", pl: 7 }}>
+                  <Typography fontWeight={700} color="grey">
+                    Total
+                  </Typography>
+                </TableCell>
+                <TableCell align="center" sx={{ width: "33.3%" }}>
+                  <Typography fontWeight={700} color="grey">
+                    {sumTotal([
+                      cdb?.percentage || 0,
+                      lci?.percentage || 0,
+                      lca?.percentage || 0,
+                      tesouro?.percentage || 0,
+                    ])}
+                    %
+                  </Typography>
+                </TableCell>
+                <TableCell align="center" sx={{ width: "33.3%" }}>
+                  <Typography fontWeight={700} color="grey">
+                    R$
+                    {sumTotal([
+                      cdb?.value || 0,
+                      lci?.value || 0,
+                      lca?.value || 0,
+                      tesouro?.value || 0,
+                    ])}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+
+              <IncomeTypesRow
+                name="CDB"
+                income={cdb}
+                setIncome={setCdb}
+                availableCash={fixedIncome?.value}
+                layer={2}
+                bgColor="whitesmoke"
+              />
+              <IncomeTypesRow
+                name="LCA"
+                income={lca}
+                setIncome={setLca}
+                availableCash={fixedIncome?.value}
+                layer={2}
+                bgColor="whitesmoke"
+              />
+              <IncomeTypesRow
+                name="LCI"
+                income={lci}
+                setIncome={setLci}
+                availableCash={fixedIncome?.value}
+                layer={2}
+                bgColor="whitesmoke"
+              />
+              <IncomeTypesRow
+                name="Tesouro Direto"
+                income={tesouro}
+                setIncome={setTesouro}
+                availableCash={fixedIncome?.value}
+                layer={2}
+                bgColor="whitesmoke"
+              />
+            </IncomeTypesRow>
+
+            <IncomeTypesRow
+              name="Renda Variavel"
+              income={variableIncome}
+              setIncome={setVariableIncome}
+              availableCash={availableCash}
+            >
+              <TableRow sx={{ background: "whitesmoke" }}>
+                <TableCell sx={{ width: "33.3%", pl: 7 }}>
+                  <Typography fontWeight={700} color="grey">
+                    Total
+                  </Typography>
+                </TableCell>
+                <TableCell align="center" sx={{ width: "33.3%" }}>
+                  <Typography fontWeight={700} color="grey">
+                    {sumTotal([
+                      etf?.percentage || 0,
+                      fii?.percentage || 0,
+                      acao?.percentage || 0,
+                    ])}
+                    %
+                  </Typography>
+                </TableCell>
+                <TableCell align="center" sx={{ width: "33.3%" }}>
+                  <Typography fontWeight={700} color="grey">
+                    R$
+                    {sumTotal([
+                      etf?.value || 0,
+                      fii?.value || 0,
+                      acao?.value || 0,
+                    ])}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+
+              <IncomeTypesRow
+                name="ETF"
+                income={etf}
+                setIncome={setEtf}
+                availableCash={variableIncome?.value}
+                layer={2}
+                bgColor="whitesmoke"
+              />
+              <IncomeTypesRow
+                name="FII"
+                income={fii}
+                setIncome={setFii}
+                availableCash={variableIncome?.value}
+                layer={2}
+                bgColor="whitesmoke"
+              />
+              <IncomeTypesRow
+                name="Ações"
+                income={acao}
+                setIncome={setAcao}
+                availableCash={variableIncome?.value}
+                layer={2}
+                bgColor="whitesmoke"
+              />
+            </IncomeTypesRow>
+          </TableBody>
+
+          <TableFooter>
+            <TableRow>
+              <TableCell sx={{ width: "33.3%" }}>
+                <Typography fontWeight={700} color="#000">
                   Total
                 </Typography>
               </TableCell>
               <TableCell align="center" sx={{ width: "33.3%" }}>
-                <Typography fontWeight={700} color="grey">
+                <Typography fontWeight={700} color="#000">
                   {sumTotal([
-                    cdb?.percentage || 0,
-                    lci?.percentage || 0,
-                    lca?.percentage || 0,
-                    tesouro?.percentage || 0,
+                    fixedIncome?.percentage || 0,
+                    variableIncome?.percentage || 0,
                   ])}
                   %
                 </Typography>
               </TableCell>
               <TableCell align="center" sx={{ width: "33.3%" }}>
-                <Typography fontWeight={700} color="grey">
+                <Typography fontWeight={700} color="#000">
                   R$
                   {sumTotal([
-                    cdb?.value || 0,
-                    lci?.value || 0,
-                    lca?.value || 0,
-                    tesouro?.value || 0,
+                    fixedIncome?.value || 0,
+                    variableIncome?.value || 0,
                   ])}
                 </Typography>
               </TableCell>
             </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
 
-            <IncomeTypesRow
-              name="CDB"
-              income={cdb}
-              setIncome={setCdb}
-              availableCash={fixedIncome?.value}
-              layer={2}
-              bgColor="whitesmoke"
-            />
-            <IncomeTypesRow
-              name="LCA"
-              income={lca}
-              setIncome={setLca}
-              availableCash={fixedIncome?.value}
-              layer={2}
-              bgColor="whitesmoke"
-            />
-            <IncomeTypesRow
-              name="LCI"
-              income={lci}
-              setIncome={setLci}
-              availableCash={fixedIncome?.value}
-              layer={2}
-              bgColor="whitesmoke"
-            />
-            <IncomeTypesRow
-              name="Tesouro Direto"
-              income={tesouro}
-              setIncome={setTesouro}
-              availableCash={fixedIncome?.value}
-              layer={2}
-              bgColor="whitesmoke"
-            />
-          </IncomeTypesRow>
-
-          <IncomeTypesRow
-            name="Renda Variavel"
-            income={variableIncome}
-            setIncome={setVariableIncome}
-            availableCash={availableCash}
-          >
-            <TableRow sx={{ background: "whitesmoke" }}>
-              <TableCell sx={{ width: "33.3%", pl: 7 }}>
-                <Typography fontWeight={700} color="grey">
-                  Total
-                </Typography>
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ minWidth: 650 }}
+          aria-label="Distribution Table"
+          size="small"
+        >
+          <TableHead sx={{ background: "#e3e3e3" }}>
+            <TableRow>
+              <TableCell sx={{ width: "33.3%" }}>
+                Tipo de investimento
               </TableCell>
               <TableCell align="center" sx={{ width: "33.3%" }}>
-                <Typography fontWeight={700} color="grey">
-                  {sumTotal([
-                    etf?.percentage || 0,
-                    fii?.percentage || 0,
-                    acao?.percentage || 0,
-                  ])}
-                  %
-                </Typography>
+                % a ser investido
               </TableCell>
               <TableCell align="center" sx={{ width: "33.3%" }}>
-                <Typography fontWeight={700} color="grey">
-                  R$
-                  {sumTotal([
-                    etf?.value || 0,
-                    fii?.value || 0,
-                    acao?.value || 0,
-                  ])}
-                </Typography>
+                Valor a ser investido
               </TableCell>
             </TableRow>
+          </TableHead>
 
-            <IncomeTypesRow
-              name="ETF"
-              income={etf}
-              setIncome={setEtf}
-              availableCash={variableIncome?.value}
-              layer={2}
-              bgColor="whitesmoke"
-            />
-            <IncomeTypesRow
-              name="FII"
-              income={fii}
-              setIncome={setFii}
-              availableCash={variableIncome?.value}
-              layer={2}
-              bgColor="whitesmoke"
-            />
-            <IncomeTypesRow
-              name="Ações"
-              income={acao}
-              setIncome={setAcao}
-              availableCash={variableIncome?.value}
-              layer={2}
-              bgColor="whitesmoke"
-            />
-          </IncomeTypesRow>
-        </TableBody>
-
-        <TableFooter>
-          <TableRow>
-            <TableCell sx={{ width: "33.3%" }}>
-              <Typography fontWeight={700} color="#000">
-                Total
-              </Typography>
-            </TableCell>
-            <TableCell align="center" sx={{ width: "33.3%" }}>
-              <Typography fontWeight={700} color="#000">
-                {sumTotal([
-                  fixedIncome?.percentage || 0,
-                  variableIncome?.percentage || 0,
-                ])}
-                %
-              </Typography>
-            </TableCell>
-            <TableCell align="center" sx={{ width: "33.3%" }}>
-              <Typography fontWeight={700} color="#000">
-                R$
-                {sumTotal([
-                  fixedIncome?.value || 0,
-                  variableIncome?.value || 0,
-                ])}
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+          <TableBody>
+            {tableContent?.map((row, index) => (
+              <IncomeRow
+                key={row?.name}
+                row={row}
+                availableCash={availableCash}
+                currentIndex={index}
+                handleChangeContent={handleChangeContent}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
